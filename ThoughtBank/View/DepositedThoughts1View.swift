06a34701d957b93e3256b584a522d8a0e0fc1106
Laksh Makhija <venomizer11@gmail.com>
2023-11-16 19:02:14 -0500

    Added DepositedThoughts1View and ThoughtView

---
+++ ThoughtBank/View/DepositedThoughts1View.swift
@@ -0,0 +1,18 @@
+//
+//  DepositedThoughts1View.swift
+//  ThoughtBank
+//
+//  Created by Laksh Makhija on 09/11/2023.
+//
+
+import SwiftUI
+
+struct DepositedThoughts1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DepositedThoughts1View()
+}
---
+++ ThoughtBank/View/ThoughtView.swift
@@ -0,0 +1,18 @@
+//
+//  ThoughtView.swift
+//  ThoughtBank
+//
+//  Created by Laksh Makhija on 09/11/2023.
+//
+
+import SwiftUI
+
+struct ThoughtView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThoughtView()
+}
